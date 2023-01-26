9abbf3efef8a513605fa22a515c3285d4a3b409d
sangitasarkar74 <sangitasarkar@gmail.com>
2023-01-26 10:59:17 +0530

    CSS done again

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,16 @@ import Search from "./Search";
 import { Link } from "react-router-dom";
 import MainPage from "./MainPage";
 import Img from "./logo-quora.png";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = ({ query, setQuery }) => {
   return (
     <div id="header">
       <img src={Img} alt="Image" />
+      <div className="search-input">
+      <SearchIcon />
       <Search setQuery={setQuery} query={query} />
+      </div>
       <ul className="nav-links">
         <Link to="/">
           <li>Home</li>
